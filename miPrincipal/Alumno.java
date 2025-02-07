cbd1e95fdd5c320d71c9d8f872e2775db80f3392
Oscar Mario Velarde Barraza <l24170769@culiacan.tecnm.mx>
2025-02-07 08:58:52 -0700

    Update Alumno.java

--- miPrincipal/Alumno.java
+++ miPrincipal/Alumno.java
@@ -1,1 +1,44 @@
+package logica;
+
+public class Alumno {
+  //Atributos
+  String nombre;
+  int num_control;
+  double promedio;
+  String carrera;
+
+  //Metodos constructores
+  //vacio
+  public Alumno(){
+
+  }
+
+  //lleno
+  public Alumno(String nombre, int num_control, double promedio, String carrera){
+
+    
+
+
+  public void cargarMaterias(){
+    System.out.println("Estoy cargando materias");
+
+  }
+
+  public boolean modificarEstatus(){
+    System.out.println("Estoy revisando historial");
+
+  }
+
+  public void revisarHistorial(){
+    System.out.println("Estoy revisando histrial");
+
+  }                   
+
+  public double obtenerPromedio(double promedio){
+    return promedio;
+
+  }
+  
+}
+
 
