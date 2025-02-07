655b0b20572a253974f03debac3c0c86f8547489
Oscar Mario Velarde Barraza <l24170769@culiacan.tecnm.mx>
2025-02-07 08:31:17 -0700

    Update Principal.java

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,10 +4,5 @@ public class Principal {
     public String getGreeting() {
         return "Buenas a todas guapisimos!";
     }
-
-    public static void main(String[] args) {
-        Principal Principal = new Principal();
-        String saludo = Principal.getGreeting();
-        System.out.println(new Principal().getGreeting());
-    }
+    
 }
